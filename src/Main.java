dd08b4b031ea40e33d7ba2641a096642e96ea07f
artur <artur@DESKTOP-87PHAM9.home>
2018-07-02 22:03:17 +0200

    Added comments and a few lines

--- src/Main.java
+++ src/Main.java
@@ -10,25 +10,34 @@ public class Main {
 	private static final DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 	
 	public static void main(String[] args) {
+		
+		//Listy zbiorników i dyspozytorów
 		Nozzle nozzle1 = new Nozzle(0);
 		List<Nozzle> nozzleList = new ArrayList<Nozzle>();
 		nozzleList.add(nozzle1);
+		List<Tank> tankList = new ArrayList<Tank>();
 		Tank tank1 = new Tank(0, nozzleList, 30000, 27000, 25, 0);
+		tankList.add(tank1);
 		
+
+		
+		//D³ugoœæ symulacji	
 		Date currentDate = new Date();
 		Calendar currentDateSet = Calendar.getInstance();
-		
-		//D³ugoœæ symulacji
 		Date endDate = new Date();
 		Calendar endDateSet = Calendar.getInstance();
 		endDateSet.setTime(endDate);
 		endDateSet.add(Calendar.DATE, 7);
 		endDate = endDateSet.getTime();
 		
+		//Zmienne wykorzystywane do symulacji
 		boolean isFueling = false;
 		boolean alreadyArrived = false;
-		int customerArrivingTime = 0;
-		int fuelingTime = 0;
+		double fuelNeeded = -1;
+		int customerArrivingTime = -1;
+		int fuelingTime = -1;
+		int whichTank = -1;
+		int whichNozzle = -1;
 		
 		while(currentDate.compareTo(endDate) < 0) {
 			//Losowanie czasu oczekiwania na klienta
@@ -36,14 +45,28 @@ public class Main {
 				customerArrivingTime = customerArrivingTime();
 				alreadyArrived = true;
 			} else if (alreadyArrived && !isFueling) {
-				fuelingTime = fuelingTime();
+				fuelNeeded = fuelNeeded();
+				fuelingTime = fuelingTime(fuelNeeded);
 				isFueling = true;
 			}
 			//Losowanie iloœci tankowanego paliwa
+			if (alreadyArrived && isFueling && customerArrivingTime <= 0 && fuelingTime >= 0) {
+				if (whichTank == -1 && whichNozzle == -1) {
+					whichTank = drawTank();
+					whichNozzle = drawNozzle();
+				}
+				//Zmiana currentVolume dla wylosowanego tanka
+			}
 			
+			//Zatrzymujemy dekrementacje na wartoœciach -1
+			if (customerArrivingTime >= 0) {
+				--customerArrivingTime;
+			}
+			if (fuelingTime >= 0) {
+				--fuelingTime;
+			}
 			
-			--customerArrivingTime;
-			--fuelingTime;
+			//Wyniki symulacji co "minutê"
 			currentDateSet.setTime(currentDate);
 			currentDateSet.add(Calendar.MINUTE, 1);
 			currentDate = currentDateSet.getTime();
@@ -53,9 +76,23 @@ public class Main {
 		
 	}
 	
-	public static int fuelingTime() {
+	//Losowanie ile klient chce kupiæ paliwa
+	public static double fuelNeeded() {
 		return 0;
 	}
+	//Losowanie z którego zbiornika chcemy zatankowaæ
+	public static int drawTank() {
+		return 0;
+	}	
+	//Losowanie który dystrybutor zostanie wykorzystany
+	public static int drawNozzle() {
+		return 0;
+	}
+	//Obliczanie ile iteracji musi przejœæ do skoñczenia tankowania na podstawie wylosowanej wartoœci "fuelNeeded"
+	public static int fuelingTime(double fuelNeeded) {
+		return 0;
+	}
+	//Losowanie czasu oczekiwania na klienta
 	public static int customerArrivingTime() {
 		return 0;
 	}
